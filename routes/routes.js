acd44302459da5ec68020abc7c2582db915c3e3d
Julia Reed <msjuliareed@gmail.com>
2017-10-31 16:23:52 -0500

    fixed NOM

--- routes/routes.js
+++ routes/routes.js
@@ -19,6 +19,7 @@ router.get("/", function(req , res) {
 router.post("/burgers/create", function(req, res) {
     db.Burger.create({
         burger_name: req.body.burger_name
+        id: req.body.id
     })
         .then(function(newBurger){
             console.log("reloading burgers");
